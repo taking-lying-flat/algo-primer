6e5692a2f2878d61e19584770a20e1944f841f4a
cherry77-cloud <1615405@qq.com>
2025-11-05 22:41:28 +0800

    Update sliding_window.py

--- two_pointers/sliding_window.py
+++ two_pointers/sliding_window.py
@@ -74,3 +74,19 @@ class SlidingWindowUtils:
             if right - left + 1 == len(p):
                 ans.append(left)         
         return ans
+
+
+    # ░░░░░░░░░░░ LeetCode 1493 —— 删掉一个元素以后全为 1 的最长子数组 ░░░░░░░░░░░
+    @staticmethod
+    def longestSubarray(nums: List[int]) -> int:
+        ans = cnt0 = left = 0
+        for right, x in enumerate(nums):
+            # 1. 入，nums[right] 进入窗口
+            cnt0 += 1 - x  # 维护窗口中的 0 的个数
+            while cnt0 > 1:  # 不符合题目要求
+                # 2. 出，nums[left] 离开窗口
+                cnt0 -= 1 - nums[left]  # 维护窗口中的 0 的个数
+                left += 1
+            # 3. 更新答案，注意不是 right-left+1，因为我们要删掉一个数
+            ans = max(ans, right - left)
+        return ans
