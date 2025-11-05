1dc99d0ad8b6d9a7fa173f0eb30d9b207d80dc5b
cherry77-cloud <1615405@qq.com>
2025-11-05 09:20:08 +0800

    Update fixed_window.py

--- two_pointers/fixed_window.py
+++ two_pointers/fixed_window.py
@@ -40,6 +40,23 @@ def fixed_window_template(nums: List[int], k: int) -> Any:
 
 
 class FixedWindowSuite:
+    # ░░░░░░░░░░░ LeetCode 643 —— 子数组最大平均数 I ░░░░░░░░░░░
+    @staticmethod
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        max_s = -inf  # 窗口元素和的最大值
+        s = 0  # 维护窗口元素和
+        for i, x in enumerate(nums):
+            # 1. 进入窗口
+            s += x
+            if i < k - 1:  # 窗口大小不足 k
+                continue
+            # 2. 更新答案
+            max_s = max(max_s, s)
+            # 3. 离开窗口
+            s -= nums[i - k + 1]
+        return max_s / k
+
+    
     # ░░░░░░░░░░░ LeetCode 1343 —— 大小为 K 且平均值大于等于阈值的子数组数目 ░░░░░░░░░░░
     @staticmethod
     def numOfSubarrays(arr: List[int], k: int, threshold: int) -> int:
