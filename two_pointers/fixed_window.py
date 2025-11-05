5ec891c807448c315f279da836f6886267110b88
cherry77-cloud <1615405@qq.com>
2025-11-05 08:41:06 +0800

    Create fixed_window.py

---
+++ two_pointers/fixed_window.py
@@ -0,0 +1,20 @@
+class Solution:
+    @staticmethod
+    def maxVowels(s: str, k: int) -> int:
+        ans = vowel = 0
+        for i, c in enumerate(s):  # 枚举窗口右端点 i
+            # 1. 右端点进入窗口
+            if c in "aeiou":
+                vowel += 1
+
+            left = i - k + 1  # 窗口左端点
+            if left < 0:      # 窗口大小不足 k，尚未形成第一个窗口
+                continue
+
+            # 2. 更新答案
+            ans = max(ans, vowel)
+
+            # 3. 左端点离开窗口，为下一个循环做准备
+            if s[left] in "aeiou":
+                vowel -= 1
+        return ans
