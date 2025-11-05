b57bda6d2d14b143830bf89503c8ceb319b774e7
cherry77-cloud <1615405@qq.com>
2025-11-05 09:02:54 +0800

    Update fixed_window.py

--- two_pointers/fixed_window.py
+++ two_pointers/fixed_window.py
@@ -3,6 +3,23 @@ from collections import defaultdict
 
 
 class FixedWindowSuite:
+    # ░░░░░░░░░░░ LeetCode 1343 —— 大小为 K 且平均值大于等于阈值的子数组数目 ░░░░░░░░░░░
+    @staticmethod
+    def numOfSubarrays(arr: List[int], k: int, threshold: int) -> int:
+        ans = s = 0  # 维护窗口元素和
+        for i, x in enumerate(arr):
+            # 1. 进入窗口
+            s += x
+            if i < k - 1:  # 窗口大小不足 k
+                continue
+            # 2. 更新答案
+            if s >= k * threshold:
+                ans += 1
+            # 3. 离开窗口
+            s -= arr[i - k + 1]
+        return ans
+
+    
     # ░░░░░░░░░░░ LeetCode 1456 —— 定长子串中元音的最大数目 ░░░░░░░░░░░
     @staticmethod
     def maxVowels(s: str, k: int) -> int:
