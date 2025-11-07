d95d54da84b7e803a16bf5609a83fe39fabeebec
cherry77-cloud <1615405@qq.com>
2025-11-07 22:43:41 +0800

    Update sliding_window.py

--- two_pointers/sliding_window.py
+++ two_pointers/sliding_window.py
@@ -256,3 +256,18 @@ class SlidingWindowUtils:
                 left += 1
             ans += left
         return ans
+
+
+    # ░░░░░░░░░░░ LeetCode 2962 —— 统计最大元素至少出现 K 次的子数组 ░░░░░░░░░░░
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        mx = max(nums)
+        ans = cnt_mx = left = 0
+        for x in nums:
+            if x == mx:
+                cnt_mx += 1
+            while cnt_mx == k:
+                if nums[left] == mx:
+                    cnt_mx -= 1
+                left += 1
+            ans += left
+        return ans
