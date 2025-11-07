90b7427cf02bcc1e3281affb29675423e54f767f
cherry77-cloud <1615405@qq.com>
2025-11-07 22:24:30 +0800

    Update sliding_window.py

--- two_pointers/sliding_window.py
+++ two_pointers/sliding_window.py
@@ -246,10 +246,10 @@ class SlidingWindowUtils:
     def numberOfSubstrings(self, s: str) -> int:
         ans = left = 0
         cnt = defaultdict(int)
-        for right, c in enumerate(s):
+        for c in s:
             cnt[c] += 1
             while len(cnt) == 3:
-                out = s[left]
+                out = s[left]  # 离开窗口的字母
                 cnt[out] -= 1
                 if cnt[out] == 0:
                     del cnt[out]
