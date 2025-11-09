444ebb4e4cee824e7ff53a392691c2eed37c9a8a
cherry77-cloud <1615405@qq.com>
2025-11-09 13:48:42 +0800

    Create grid_search.py

---
+++ search/grid_search.py
@@ -0,0 +1,21 @@
+class GridSearchToolkit:
+    # ░░░░░░░░░░░░░░░░░░░ LeetCode 79 —— 单词搜索 ░░░░░░░░░░░░░░░░░░░
+    def exist(board: List[List[str]], word: str) -> bool:
+        cnt = Counter(c for row in board for c in row)
+        if not cnt >= Counter(word):
+            return False
+        if cnt[word[-1]] < cnt[word[0]]:
+            word = word[::-1]
+        m, n = len(board), len(board[0])
+        def dfs(i: int, j: int, k: int) -> bool:
+            if board[i][j] != word[k]:  # 匹配失败
+                return False
+            if k == len(word) - 1:      # 匹配成功, 处理单字符情况
+                return True
+            board[i][j] = ''            # 标记访问过
+            for x, y in (i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j):  # 相邻格子
+                if 0 <= x < m and 0 <= y < n and dfs(x, y, k + 1):
+                    return True  # 搜到了！
+            board[i][j] = word[k]  # 恢复现场
+            return False  # 没搜到
+        return any(dfs(i, j, 0) for i in range(m) for j in range(n))
