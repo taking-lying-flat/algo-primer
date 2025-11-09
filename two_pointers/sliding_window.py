a90e092abb9958c755ecc06928fa955a4f37b51c
cherry77-cloud <1615405@qq.com>
2025-11-09 17:26:21 +0800

    Update sliding_window.py

--- two_pointers/sliding_window.py
+++ two_pointers/sliding_window.py
@@ -11,6 +11,20 @@ class SlidingWindowUtils:
             ans = max(ans, right - left + 1)
         return ans
 
+
+    # ░░░░░░░░░░░░░░░ LeetCode 209 —— 长度最小的子数组 ░░░░░░░░░░░░░░░
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        ans = n + 1
+        s = left = 0
+        for right, x in enumerate(nums):  # 枚举子数组右端点
+            s += x
+            while s >= target:  # 满足要求
+                ans = min(ans, right - left + 1)
+                s -= nums[left]
+                left += 1  # 左端点右移
+        return ans if ans <= n else 0
+
     
     # ░░░░░░░░░░░ LeetCode 438 —— 找到字符串中所有字母异位词 ░░░░░░░░░░░
     def findAnagrams(self, s: str, p: str) -> List[int]:
