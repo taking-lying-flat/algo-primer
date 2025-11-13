ee91720aa24ddd7d73fe05bb2f682f2f6aa33cc8
cherry77-cloud <1615405@qq.com>
2025-11-13 10:39:04 +0800

    Update inplace_edit.py

--- pointer/inplace_edit.py
+++ pointer/inplace_edit.py
@@ -1,6 +1,8 @@
 class InPlaceModifySuite:
     # ░░░░░░░░░░░ LeetCode 2273 —— 移除字母异位词后的结果数组 ░░░░░░░░░░░
-    def removeAnagrams(self, words: List[str]) -> List[str]:
+    def removeAnagrams(
+        self, words: List[str]
+    ) -> List[str]:
         k = 1
         for s, t in pairwise(words):
             if sorted(s) != sorted(t):
