926e96031a38d9c6775a806e3e4e72d54ddbdc9e
cherry77-cloud <1615405@qq.com>
2025-11-20 08:00:49 +0800

    Update inplace.py

--- pointer/inplace.py
+++ pointer/inplace.py
@@ -1,12 +1,29 @@
 class InPlaceModifySuite:
     # ░░░░░░░░░░░░░░ LeetCode 26 —— 删除有序数组中的重复项 ░░░░░░░░░░░░░░
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(
+        self, nums: List[int]
+    ) -> int:
         k = 1
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
                 nums[k] = nums[i]
                 k += 1
         return k
+
+
+    # ░░░░░░░░░░░░░░ LeetCode 75 —— 颜色分类 ░░░░░░░░░░░░░░
+    def sortColors(
+        self, nums: List[int]
+    ) -> None:
+        p0 = p1 = 0
+        for i, x in enumerate(nums):
+            nums[i] = 2
+            if x <= 1:
+                nums[p1] = 1
+                p1 += 1
+            if x == 0:
+                nums[p0] = 0
+                p0 += 1
 
     
     # ░░░░░░░░░░░ LeetCode 2273 —— 移除字母异位词后的结果数组 ░░░░░░░░░░░
